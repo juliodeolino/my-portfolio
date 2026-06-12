e9462f117e10b25e8ecf4526d93dfc8613153529
juliodeolino <juliodeolino07@gmail.com>
2026-06-12 15:06:05 -0300

    feat: imgs projects

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -17,7 +17,7 @@ const PROJETOS_DATA = [
     description:
       "E-commerce moderno de artigos esportivos com design mobile-first e totalmente responsivo. Desenvolvido em React e estilizado com Tailwind CSS, focado em performance e experiência de navegação fluida em qualquer dispositivo. ",
     tags: [ "React", "Tailwind" ],
-    imageUrl: "../src/assets/ecommerce-materiais-esportivos.png",
+    imageUrl: "https://qs2ptfvjv0j5xteh.private.blob.vercel-storage.com/ecommerce-materiais-esportivos.png?vercel-blob-valid-until=1781287528514&vercel-blob-delegation=eyJzdG9yZUlkIjoic3RvcmVfUVMyUHRGVmp2MEo1WHRlaCIsIm93bmVySWQiOiJ0ZWFtX1Q4UEx1RkJkaU9TRHhGV0ZaUnBuQ2liRiIsInBhdGhuYW1lIjoiKiIsIm9wZXJhdGlvbnMiOlsiZ2V0IiwiaGVhZCJdLCJ2YWxpZFVudGlsIjoxNzgxMzMwNjA3MzE1LCJpYXQiOjE3ODEyODc0MDc0Mjh9.FaYVpw8md8ghR4Uue7DJTKif__nz7LDWnq6ZNMD32Xg&vercel-blob-signature=sW-PI2Y5yllDe9L7TeQtTsHTjyDFHOI6QnFQk25DfGg",
     githubUrl: "https://github.com/juliodeolino/Ecommerce-Materiais-Esportivos",
   },
   {
@@ -26,7 +26,7 @@ const PROJETOS_DATA = [
     description:
       "Sistema inteligente que analisa tabelas em PDF, extrai dados de produtos e utiliza IA (Google Gemini) para comparar preços automaticamente. Gera uma planilha organizada com o comparativo final, otimizando decisões de compra",
     tags: [ "Python", "Gemini", "Pandas"],
-    imageUrl: "../src/assets/comparador-deprecos-ia.png",
+    imageUrl: "https://qs2ptfvjv0j5xteh.private.blob.vercel-storage.com/comparador-deprecos-ia.png?vercel-blob-valid-until=1781287513887&vercel-blob-delegation=eyJzdG9yZUlkIjoic3RvcmVfUVMyUHRGVmp2MEo1WHRlaCIsIm93bmVySWQiOiJ0ZWFtX1Q4UEx1RkJkaU9TRHhGV0ZaUnBuQ2liRiIsInBhdGhuYW1lIjoiKiIsIm9wZXJhdGlvbnMiOlsiZ2V0IiwiaGVhZCJdLCJ2YWxpZFVudGlsIjoxNzgxMzMwNjA3MzE1LCJpYXQiOjE3ODEyODc0MDc0Mjh9.FaYVpw8md8ghR4Uue7DJTKif__nz7LDWnq6ZNMD32Xg&vercel-blob-signature=VFadLAxZM4KOG7oIS-ki16oqknds9-n7rMmBbubTaQk",
     githubUrl: "https://github.com/juliodeolino/comparador-tabelas-ia"
   },
   {
@@ -34,7 +34,7 @@ const PROJETOS_DATA = [
     title: "Blog",
     description: "Plataforma de blog completa com back-end em Node.js e banco de dados MongoDB. Sistema robusto para criação, gerenciamento e exibição de posts, com estrutura escalável e pronta para receber autenticação e comentários.",
     tags: [ "JavaScript", "Node.js", "Express", "MongoDB"],
-    imageUrl: "../src/assets/Blog.png",
+    imageUrl: "https://qs2ptfvjv0j5xteh.private.blob.vercel-storage.com/Blog.png?vercel-blob-valid-until=1781287577125&vercel-blob-delegation=eyJzdG9yZUlkIjoic3RvcmVfUVMyUHRGVmp2MEo1WHRlaCIsIm93bmVySWQiOiJ0ZWFtX1Q4UEx1RkJkaU9TRHhGV0ZaUnBuQ2liRiIsInBhdGhuYW1lIjoiKiIsIm9wZXJhdGlvbnMiOlsiZ2V0IiwiaGVhZCJdLCJ2YWxpZFVudGlsIjoxNzgxMzMwNjA3MzE1LCJpYXQiOjE3ODEyODc0MDc0Mjh9.FaYVpw8md8ghR4Uue7DJTKif__nz7LDWnq6ZNMD32Xg&vercel-blob-signature=uIRwc2zXZ730r_BXB4Si5xT6hc_G7WzojXM2sj8M47A",
     githubUrl: "https://github.com/juliodeolino/Blog-NodeJs"
   },
   {
@@ -43,7 +43,7 @@ const PROJETOS_DATA = [
     description:
       "Aplicação em Java para gerenciamento de tarefas com operações CRUD: criar, excluir, editar e listar tarefas. Projeto focado em lógica de programação, manipulação de coleções e persistência de dados.",
     tags: ["Java"],
-    imageUrl: "../src/assets/backend.jpg",
+    imageUrl: "https://qs2ptfvjv0j5xteh.private.blob.vercel-storage.com/projetos%20sem%20front.jpg?vercel-blob-valid-until=1781287544067&vercel-blob-delegation=eyJzdG9yZUlkIjoic3RvcmVfUVMyUHRGVmp2MEo1WHRlaCIsIm93bmVySWQiOiJ0ZWFtX1Q4UEx1RkJkaU9TRHhGV0ZaUnBuQ2liRiIsInBhdGhuYW1lIjoiKiIsIm9wZXJhdGlvbnMiOlsiZ2V0IiwiaGVhZCJdLCJ2YWxpZFVudGlsIjoxNzgxMzMwNjA3MzE1LCJpYXQiOjE3ODEyODc0MDc0Mjh9.FaYVpw8md8ghR4Uue7DJTKif__nz7LDWnq6ZNMD32Xg&vercel-blob-signature=PiLNsB6vUrmST4_C7DoyvCRrad0YA-BM52hsYYMXoB8",
     githubUrl: "https://github.com/juliodeolino/Task-manager"
   }
 ];
